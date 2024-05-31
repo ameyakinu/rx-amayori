22eeeec9a72ff428c47bc696700b42e787ef93cb
AmeYaki <am7777336@gmail.com>
2024-05-30 22:22:25 -0500

    ajustando la calidad x2

--- main.js
+++ main.js
@@ -26,20 +26,18 @@ reloadCommandsUpdateToSlash();
 client.login(process.env.token).then(async () => {
   console.log(`${client.user.displayName} is connect!`);
 });
-
 // this is the entrypoint for discord-player based application
 const player = new Player(client, {
   ytdlOptions: {
-    filter: "audioonly", // Solo audio para reducir el uso de datos
+    filter: "audioonly", // Solo descarga el audio
     quality: "lowestaudio", // La calidad más baja de audio disponible
     highWaterMark: 1 << 18, // Tamaño del buffer a 256 KB
     dlChunkSize: 64 * 1024, // Fragmentos de descarga de 64 KB
     requestOptions: {
-      // Aquí puedes agregar opciones adicionales si necesitas usar un proxy o encabezados específicos
+      // Opciones adicionales para el módulo request si es necesario
     },
     liveBuffer: 10000, // Tamaño del buffer para streams en vivo a 10 segundos
     begin: "0s", // Empezar desde el principio del video
-    range: { start: 0, end: 256000 }, // Descargar solo los primeros 256 KB (puedes ajustar según necesidad)
   },
   quality: "low", // Calidad de streaming baja
   autoSelfDeaf: true, // Auto silenciar el bot al unirse a un canal de voz
