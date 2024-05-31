4b9a2d3f29223f2b53d9bb9dcdbe5b46234ca0bb
AmeYaki <am7777336@gmail.com>
2024-05-30 23:13:47 -0500

    error fix autocomplete

--- src/commands/music/play.js
+++ src/commands/music/play.js
@@ -18,43 +18,54 @@ export default {
   async autocomplete(interaction) {
     const player = useMainPlayer();
     const query = interaction.options.getString("query", true);
-    const results = await player.search(query);
 
-    // Discord's character limit for autocompletion responses is 100 characters per option name.
-    const MAX_LENGTH = 100;
+    if (!query) {
+      return interaction.respond([]);
+    }
 
-    return interaction.respond(
-      results.tracks.slice(0, 10).map((t) => {
-        let name = `🎵 ${t.title}`;
-        if (name.length > MAX_LENGTH) {
-          name = `${name.substring(0, MAX_LENGTH - 3)}...`;
-        }
-        return {
-          name: name,
-          value: t.url,
-        };
-      })
-    );
+    try {
+      const results = await player.search(query);
+
+      // Discord's character limit for autocompletion responses is 100 characters per option name.
+      const MAX_LENGTH = 100;
+
+      return interaction.respond(
+        results.tracks.slice(0, 10).map((t) => {
+          let name = `🎵 ${t.title}`;
+          if (name.length > MAX_LENGTH) {
+            name = `${name.substring(0, MAX_LENGTH - 3)}...`;
+          }
+          return {
+            name: name,
+            value: t.url,
+          };
+        })
+      );
+    } catch (error) {
+      console.error(error);
+      return interaction.respond([]);
+    }
   },
   async run(interaction) {
     const player = useMainPlayer();
     const channel = interaction.member.voice.channel;
-    if (!channel)
-      return interaction.reply("You are not connected to a voice channel!"); // make sure we have a voice channel
-    const query = interaction.options.getString("query", true); // we need input/query to play
+    if (!channel) {
+      return interaction.reply("You are not connected to a voice channel!");
+    }
+
+    const query = interaction.options.getString("query", true);
 
     try {
       const { track } = await player.play(channel, query, {
         nodeOptions: {
-          // nodeOptions are the options for guild node (aka your queue in simple words)
-          metadata: interaction, // we can access this metadata object using queue.metadata later on
+          metadata: interaction,
         },
       });
 
-      //return interaction.reply({ content: `**${track.title}** enqueued!` });
+      return interaction.reply(`**${track.title}** enqueued!`);
     } catch (e) {
-      // let's return error if something failed
-      return interaction.reply(`Something went wrong: ${e}`);
+      console.error(e);
+      return interaction.reply(`Something went wrong: ${e.message}`);
     }
   },
 };
